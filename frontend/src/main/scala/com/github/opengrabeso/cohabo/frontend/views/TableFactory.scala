2ae5c177bab47918f0a497827e98fa7c684c8a92
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-02-29 21:18:48 +0100

    Clickable table rows

--- frontend/src/main/scala/com/github/opengrabeso/cohabo/frontend/views/TableFactory.scala
+++ frontend/src/main/scala/com/github/opengrabeso/cohabo/frontend/views/TableFactory.scala
@@ -4,10 +4,10 @@ package frontend.views
 import common.css._
 import io.udash._
 import io.udash.bindings.modifiers.Binding.NestedInterceptor
-import org.scalajs.dom.{Element, Node}
-import org.scalajs.dom.Node
+import org.scalajs.dom.{Element, Event, Node}
 import io.udash.css.CssView._
 import io.udash.properties.ModelPropertyCreator
+import io.udash.wrappers.jquery.jQ
 import scalatags.JsDom.all._
 
 object TableFactory {
@@ -29,16 +29,31 @@ object TableFactory {
     }
   }.render
 
-  def rowFactory[ItemType: ModelPropertyCreator](attribs: Seq[TableAttrib[ItemType]]): (CastableProperty[ItemType], NestedInterceptor) => Element = (el,_) => tr(
-    produceWithNested(el) { (ha, nested) =>
-      attribs.flatMap { a =>
-        // existing but empty shortName means the column should be hidden on narrow view
-        if (a.shortName.contains("")) {
-          td(s.wideMedia, a.value(ha, el.asModel, nested)).render
-        } else {
-          td(a.value(ha, el.asModel, nested)).render
+  def rowFactory[ItemType: ModelPropertyCreator](attribs: Seq[TableAttrib[ItemType]]): (CastableProperty[ItemType], NestedInterceptor) => Element = { (el,_) =>
+    tr(
+      produceWithNested(el) { (ha, nested) =>
+        attribs.flatMap { a =>
+          // existing but empty shortName means the column should be hidden on narrow view
+          if (a.shortName.contains("")) {
+            td(s.wideMedia, a.value(ha, el.asModel, nested)).render
+          } else {
+            td(a.value(ha, el.asModel, nested)).render
+          }
         }
+      },
+      onclick :+= { e: Event =>
+        // find a checkbox and make it selected
+        val td = e.target.asInstanceOf[Element]
+        // e.target may be a td inside of tr, we need to find a tr parent in such case
+
+        val tr = jQ(td).closest("tr")
+        // TODO: some more reliable checkbox binding
+        val checkbox = tr.find("input[type='checkbox']")
+        println(s"Row clicked $td:$tr, checkbox $checkbox")
+        checkbox.trigger("click")
+
+        false
       }
-    }
-  ).render
+    ).render
+  }
 }
