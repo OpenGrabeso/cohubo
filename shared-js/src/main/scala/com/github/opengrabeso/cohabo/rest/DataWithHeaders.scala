2cc5911db3b7a3bbe08988223083a6ce86eb8db4
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-03-11 21:46:50 +0100

    Refactor: move linkHeaders to the enclosing object

--- shared-js/src/main/scala/com/github/opengrabeso/cohabo/rest/DataWithHeaders.scala
+++ shared-js/src/main/scala/com/github/opengrabeso/cohabo/rest/DataWithHeaders.scala
@@ -8,21 +8,22 @@ case class DataWithHeaders[T](data: T, paging: Map[String, String], lastModified
 
 object DataWithHeaders {
 
+  // https://developer.github.com/v3/guides/traversing-with-pagination/
+  /*
+  Link: <https://api.github.com/search/code?q=addClass+user%3Amozilla&per_page=50&page=2>; rel="next",
+  <https://api.github.com/search/code?q=addClass+user%3Amozilla&per_page=50&page=20>; rel="last"
+  */
+  def linkHeaders(linkHeader: Option[String]): Map[String, String] = {
+    linkHeader.map { l =>
+      val extract = """<([^>]*)>; *rel="([^"]*)"""".r
+      extract.findAllMatchIn(l).map { m =>
+        m.group(2) -> m.group(1)
+      }.toMap
+    }.getOrElse(Map.empty)
+  }
+
   trait Implicits {
 
-    // https://developer.github.com/v3/guides/traversing-with-pagination/
-    /*
-    Link: <https://api.github.com/search/code?q=addClass+user%3Amozilla&per_page=50&page=2>; rel="next",
-    <https://api.github.com/search/code?q=addClass+user%3Amozilla&per_page=50&page=20>; rel="last"
-    */
-    def linkHeaders(linkHeader: Option[String]): Map[String, String] = {
-      linkHeader.map { l =>
-        val extract = """<([^>]*)>; *rel="([^"]*)"""".r
-        extract.findAllMatchIn(l).map { m =>
-          m.group(2) -> m.group(1)
-        }.toMap
-      }.getOrElse(Map.empty)
-    }
 
     implicit def fromResponse[T](implicit fromBody: AsReal[HttpBody, Seq[T]]): AsReal[RestResponse, DataWithHeaders[Seq[T]]] = AsReal.create {
       resp =>
