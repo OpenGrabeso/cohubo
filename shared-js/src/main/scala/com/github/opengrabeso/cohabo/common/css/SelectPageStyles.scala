fbc641343efc5b59644ab02d45f52da974beba8c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-03-30 15:49:58 +0200

    Improve code quoting formatting.

--- shared-js/src/main/scala/com/github/opengrabeso/cohabo/common/css/SelectPageStyles.scala
+++ shared-js/src/main/scala/com/github/opengrabeso/cohabo/common/css/SelectPageStyles.scala
@@ -148,8 +148,6 @@ object SelectPageStyles extends CssBase {
   val articleMarkdown = style(
     // inspired by GitHub css
     unsafeRoot(".article-content") (
-      unsafeChild("pre") (
-      ),
       unsafeChild("blockquote") (
         padding(`0`, 1 em),
         color(c"#6a737d"),
@@ -191,8 +189,55 @@ object SelectPageStyles extends CssBase {
         unsafeChild("tr:nth-child(2n)") (
           backgroundColor(c"#f6f8fa"),
         )
-      )
+      ),
+      unsafeChild(".lh-condensed") (
+        lineHeight(1.25).important
+      ),
+      unsafeChild(".bg-gray-light") (
+        backgroundColor(c"#fafbfc").important
+      ),
+      unsafeChild(".js-file-line-container") (
+        unsafeChild(".tab-size[data-tab-size=\"8\"]") (
+          tabSize := "8"
+        )
+      ),
+      unsafeChild(".js-line-number") (
+      ),
+      unsafeChild(".blob-num") (
+        width(1 %%),
+        minWidth(50 px),
+        textAlign.right,
+        whiteSpace.nowrap,
+        verticalAlign.top,
+        fontFamily :=! "SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace",
+        fontSize(12 px),
+        lineHeight(20 px)
+      ),
+      unsafeChild(".blob-num:before") (
+        content := "attr(data-line-number)"
+      ),
 
+      unsafeChild(".blob-code-inner") (
+        fontFamily :=! "SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace",
+        fontSize(12 px),
+        color(c"#24292e"),
+        wordWrap.normal,
+        whiteSpace.pre
+      ),
+      unsafeChild(".js-file-line") (
+      ),
+      unsafeChild(".pl-k") (
+        color(c"#d73a49")
+      ),
+      unsafeChild(".pl-en") (
+        color(c"#6f42c1")
+      ),
+      unsafeChild(".pl-pds, .pl-s, .pl-s .pl-pse .pl-s1, .pl-sr, .pl-sr .pl-cce, .pl-sr .pl-sra, .pl-sr .pl-sre") (
+        color(c"#032f62")
+      ),
+      unsafeChild(".pl-3, .px-3") (
+        paddingLeft(16 px).important
+      )
     )
   )
 
