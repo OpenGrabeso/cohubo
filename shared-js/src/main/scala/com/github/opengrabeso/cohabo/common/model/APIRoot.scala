ecbaaf43d6b0a3f480df48dcd289cc274a2c7550
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-04-20 14:42:38 +0200

    Remove APIRoot (never used)

--- shared-js/src/main/scala/com/github/opengrabeso/cohabo/common/model/APIRoot.scala
+++
@@ -1,8 +0,0 @@
-package com.github.opengrabeso.cohabo
-package common.model
-
-import rest.EnhancedRestDataCompanion
-
-case class APIRoot()
-
-object APIRoot extends EnhancedRestDataCompanion[APIRoot]
--- shared-js/src/main/scala/com/github/opengrabeso/cohabo/rest/RestAPI.scala
+++ shared-js/src/main/scala/com/github/opengrabeso/cohabo/rest/RestAPI.scala
@@ -9,9 +9,6 @@ import common.model._
 import scala.concurrent.Future
 
 trait RestAPI {
-  @GET(path="")
-  def api: Future[APIRoot]
-
   @Prefix("")
   def authorized(@Header("Authorization") bearer: String): AuthorizedAPI
 }
